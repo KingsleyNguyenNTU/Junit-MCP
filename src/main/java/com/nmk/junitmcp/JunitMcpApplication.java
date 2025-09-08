0d17e9d56ba140421de55b90558dc59d44a363f3
Kingsley Nguyen <kingsley.nguyen@adnovum.sg>
2025-09-08 13:11:09 +0800

    Initial commit

---
+++ src/main/java/com/nmk/junitmcp/JunitMcpApplication.java
@@ -0,0 +1,13 @@
+package com.nmk.junitmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmk/junitmcp/JunitMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmk.junitmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
